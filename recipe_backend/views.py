2370ebbad4f27cd97019c098e604441ce584d741
Anshul Mathur <anshmathur00@gmail.com>
2017-06-11 23:30:23 -0700

    added initial recipe

--- recipe_backend/views.py
+++ recipe_backend/views.py
@@ -3,13 +3,27 @@ from django.http import HttpResponse
 from . import verify as vf
 from django.views.decorators.csrf import csrf_exempt
 import json
+import requests
 
 def help_response(json_response):
 	response = {}
 	response['shouldEndSession'] = False
 	response['outputSpeech'] = {'type' : "PlainText", 'text' : "Hi! This is Recipes Guru! - you can use to me cook interactively!", 'ssml' : ''}
 	json_response['response'] = response
-	return json.dumps(json_response)
+	return check_intent(json_response)
+
+def get_recipe(recipeName):
+	web_response = ("http://www.simplyrecipes.com/?s="+recipeName)
+
+def check_intent(json_response):
+	intent = json_response['intent']
+	if (intent['name'] == 'getRecipe'):
+		recipeSlots = intent['slots']
+		if ('risotto' in recipeSlots):
+			response = {'type' : "PlainText", 'text' : "1. Heat the clam juice and water. 2. Sauté shallots. 3. Add the rice to the pot. Stir-fry the rice for 2-3 minutes. 4. Add white wine and stir. 5. Add two ladles of clam juice water mixture. 6 Stirring almost constantly, let this liquid reduce until it is almost gone, then add another ladle of broth. 7. Now add in the shrimp, the parsley, and the remaining tablespoon of butter"}
+		json_response['response'] = response
+		return json.dumps(json_response)
+
 
 # Create your views here.
 @csrf_exempt
