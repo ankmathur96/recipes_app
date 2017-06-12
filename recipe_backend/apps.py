8bfcbcccb48b9932d99959a66c73a3a559b29de0
Ankit Mathur <ankitmathur@berkeley.edu>
2017-06-11 19:06:01 -0700

    first commit

---
+++ recipe_backend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RecipeBackendConfig(AppConfig):
+    name = 'recipe_backend'
---
+++ recipe_backend/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+def help_response(json_response):
+	response = {}
+	response['shouldEndSession'] = False
+	response['outputSpeech'] = {'type' : "PlainText", 'text' : "Hi! This is Recipes Guru! - you can use to me cook interactively!", 'ssml' : ''}
+	json_response['outputSpeech'] = response
+	return json.dumps(json_response)
+
+# Create your views here.
+@csrf_exempt
+def respond(request):
+	request_body = json.loads(request.body)
+	intent = request_body['request']
+	json_response = {'version' : '1.0'}
+	if (intent['type'] == 'LaunchRequest'):
+		json_response = help_response(json_response)
+	elif (intent['type'] == 'IntentRequest'):
+		json_response =  help_response(json_response)
+	elif (intent['type'] == "SessionEndedRequest"):
+		json_response = help_response(json_response)
+	
+	response = HttpResponse(status=200)
+	response['Content-Type'] = 'application/json'
+	print('LOG RESPONSE: ', json_response)
+	response.content = json_response
+	print('LOG RESPONSE: ', response)
+	return response
