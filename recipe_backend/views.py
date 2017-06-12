d896f481e7c084850cd5fceb4b90c25641ceb80b
Anshul Mathur <anshmathur00@gmail.com>
2017-06-11 23:45:50 -0700

    small fixes

--- recipe_backend/views.py
+++ recipe_backend/views.py
@@ -10,12 +10,13 @@ def help_response(json_response):
 	response['shouldEndSession'] = False
 	response['outputSpeech'] = {'type' : "PlainText", 'text' : "Hi! This is Recipes Guru! - you can use to me cook interactively!", 'ssml' : ''}
 	json_response['response'] = response
-	return check_intent(json_response)
+	return json.dumps(json_response)
 
 def get_recipe(recipeName):
 	web_response = ("http://www.simplyrecipes.com/?s="+recipeName)
 
 def check_intent(json_response, request):
+	response['shouldEndSession'] = False
 	intent = request['intent']
 	if (intent['name'] == 'getRecipe'):
 		recipeSlots = intent['slots']
@@ -23,6 +24,10 @@ def check_intent(json_response, request):
 			response = {'type' : "PlainText", 'text' : "1. Heat the clam juice and water. 2. Saute shallots. 3. Add the rice to the pot. Stir-fry the rice for 2-3 minutes. 4. Add white wine and stir. 5. Add two ladles of clam juice water mixture. 6 Stirring almost constantly, let this liquid reduce until it is almost gone, then add another ladle of broth. 7. Now add in the shrimp, the parsley, and the remaining tablespoon of butter"}
 		json_response['response'] = response
 		return json.dumps(json_response)
+	else
+		response = {'type': "PlainText", 'text': "Sorry. I am unable to tell you a recipe for that"}
+		json_response['response'] = response
+		return json.dumps(json_resonse) 
 
 
 # Create your views here.
@@ -35,7 +40,7 @@ def respond(request):
 	if (intent['type'] == 'LaunchRequest'):
 		json_response = help_response(json_response)
 	elif (intent['type'] == 'IntentRequest'):
-		json_response =  help_response(json_response, intent)
+		json_response =  check_intent(json_response, intent)
 	elif (intent['type'] == "SessionEndedRequest"):
 		json_response = help_response(json_response)
 
